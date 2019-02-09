c02f55e5fe52bf8e6b269d7de3b601a9eedb2f5d
flokol120 <flo@xn--drr-sna.site>
2019-02-09 19:53:28 +0100

    :fork_and_knife: forked the mining task

---
+++ src/jitcoin/mine.ts
@@ -0,0 +1,36 @@
+import { createHash } from 'crypto';
+
+if(process !== undefined){
+    process.on('message', async (message: any) => {
+        process.send = process.send || (() => {});
+        const steps = message.steps;
+        let nonce = (message.startingNonce as number) - steps;
+        const data = message.data;
+        const zeroCount = message.zeroCount;
+        let hash = '';
+        while (hash.substring(0, zeroCount) !== getZeroString(zeroCount)) {
+          // incrementing the nonce | init value is -1
+          nonce += steps;
+          //appendFileSync('./test.txt', nonce + ', ');
+          // data of the block is being hashed with the nonce
+          hash = createHash('sha512')
+            .update(data + nonce)
+            .digest()
+            .toString('hex');
+          // just some ouptut...
+          /*if (nonce % 10000 === 0) {
+            log(nonce + '. hash: ' + hash);
+          }*/
+        }
+        console.log('nonce found! It\'s ' + nonce);
+        process.send({ nonce, hash });
+    });
+}
+
+const getZeroString = (zeroCount: number): string => {
+  let ret = '';
+  for (let i = 0; i < zeroCount; i++) {
+    ret += '0';
+  }
+  return ret;
+};
---
+++ src/misc/interfaces.ts
@@ -0,0 +1,22 @@
+/**
+ * @author Flo Dörr, Tim Weiß
+ * @date 2019-02-06
+ */
+
+export interface BlockHeader {
+    version: number;
+    previousBlockHash: string;
+    merkleTree: string;
+    time: number;
+    nonce: number;
+}
+
+export interface BlockBody {
+    transactions: [string];
+}
+
+export interface TransactionElement {
+    userId: string;
+    randomHash: string;
+    amount: number;
+}
