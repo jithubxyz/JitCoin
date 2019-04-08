157ba9c09160c639e5cccd1863f933f61205eff9
Tim W <tim@weiss.moe>
2019-04-08 11:09:43 +0200

    🎏 Get clients and regular trackers
    Co-Authored-By: Bruno Scheufler <brunoscheufler@users.noreply.github.com>

--- src/p2p/tracker.ts
+++ src/p2p/tracker.ts
@@ -1,4 +1,4 @@
-import { get } from 'got';
+import fetch from 'node-fetch';
 
 export interface Tracker {
 	name: string;
@@ -6,14 +6,18 @@ export interface Tracker {
 	address: string;
 }
 
-interface Timings {
-    response: number | null;
-    start: number;
+export interface Client {
+    walletId: string;
+    lastHeartbeat: number;
+}
+
+interface LatencyResult {
+    tracker: Tracker;
+    latency: number;
 }
 
 async function getTrackers(rootTrackerUrl: string) {
-    const response = await get(`${rootTrackerUrl}/trackers`, { json: true });
-    const { trackers } = response.body;
+    const { trackers } = await fetch(`${rootTrackerUrl}/trackers`, { method: "GET" }).then(r => r.json());
     return trackers as Tracker[];
 }
 
@@ -21,8 +25,9 @@ async function checkLatencies(trackers: Tracker[]) {
     const stat: Array<{ tracker: Tracker, latency: number }> = [];
 
     for (const t of trackers) {
-        const response = await get(t.address);
-        const { start: startTime, response: responseTime } = ((response as any).timings as Timings);
+        const startTime = Date.now();
+        await fetch(t.address, { method: 'GET' });
+        const responseTime = Date.now();
         
         if (responseTime !== null) {
             stat.push({ tracker: t, latency: responseTime - startTime });
@@ -32,7 +37,29 @@ async function checkLatencies(trackers: Tracker[]) {
     return stat;
 }
 
-async function connect(rootTrackerUrl: string) {
-    const trackers = await getTrackers(rootTrackerUrl);
+async function getTrackerClients(tracker: Tracker): Promise<Client[]> {
+    const { clients } = await fetch(`${tracker.address}/clients`, { method: 'GET' }).then(r => r.json());
+    return clients;
+}
 
+export async function getClients(rootTrackerUrl: string) {
+    const trackers = await fetchTrackers(rootTrackerUrl);
+    const clients = (await Promise.all(trackers.map(t => getTrackerClients(t.tracker)))).flat();
+
+}
+
+async function fetchTrackers(rootTrackerUrl: string) {
+    const trackers = await getTrackers(rootTrackerUrl);
+    const latencies = await checkLatencies(trackers);
+    return latencies.sort(sortLatency);
+}
+
+function sortLatency(a: LatencyResult, b: LatencyResult) {
+    if (a.latency > b.latency) {
+        return 1;
+    }
+    if (a.latency === b.latency) {
+        return 0;
+    }
+    return -1;
 }
